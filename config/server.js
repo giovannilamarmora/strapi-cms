dfb8e0211fe0ce3e811ddad664e4830af6a9aedd
Giovanni <giovannilamarmora.working@gmail.com>
2024-03-12 19:24:17 +0100

    feat() Test

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "a56A3tQiHZUI35pMSQQo8g==",
+      "4eUPSM/0qEs88vBS2MdhvA==",
+      "g6VAXswsjhxhBop7RJsocQ==",
+      "9Cf4iI/KT9Ce2gldx/rMmQ==",
+    ]),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
